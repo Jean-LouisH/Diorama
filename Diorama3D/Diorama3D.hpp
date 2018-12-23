4a147e3722eea53b8729de2d8096a7d4fe0c5a62
Luleivon <Louis_Ml@live.co.uk>
2018-12-23 16:45:35 -0400

    Added ThirdParty for SDL, GLM, GLEW, and TinyGLTF

---
+++ Diorama3D/Diorama3D.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <SDL.h>
+#include <GL/glew.h>
+#include <tiny_gltf.h>
+#include <glm.hpp>
---
+++ Diorama3D/main.cpp
@@ -0,0 +1,7 @@
+#include "Diorama3D.hpp"
+
+int main(int argc, char* argv[])
+{
+
+	return 0;
+}
